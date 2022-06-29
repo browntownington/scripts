02ac8b023291ef538be17466f8f91404bbc52b26
browntownington <github@123.mrlb.com.au>
2022-06-29 16:27:32 +1000

    Update memory-check-onboot.sh

--- memory-check-onboot.sh
+++ memory-check-onboot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+minmem=15000
 counterpath="/mnt/apps/scripts/memory-check-onboot-counter.log"
 logpath="/mnt/apps/scripts/memory-check-onboot.log"
 logmsg=$(date +"%D %T")" || "$(uptime)
@@ -8,7 +9,7 @@ counter=$(<$counterpath)
 ((counter++))
 echo $counter > $counterpath
 mem=$(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))
-if [ $mem -gt 15000 ];
+if [ $mem -gt $minmem ];
 then
 logmsg=$(date +"%D %T")" || Detected memory is $mem. It took $counter reboots to to detect this. Continuing to boot..."
 echo $logmsg
@@ -18,15 +19,16 @@ else
 if [ $counter -gt 9 ];
 then
 echo "0"> $counterpath
-logmsg=$(date +"%D %T")" || Detected memory $mem is less than desired 15000. The script has rebooted the system $counter times to resolve this previously. The system will contine to boot with less than desired memory.";
+logmsg=$(date +"%D %T")" || Detected memory $mem is less than desired 15000. The script has rebooted the system $counter times to resolve this >
 echo $logmsg
 echo $logmsg>> $logpath
 exit 1
 else
 echo $counter> $counterpath
-logmsg=$(date +"%D %T")" || Detected memory $mem is less than desired 15000. The script has rebooted the system $counter times to resolve this previously. This system will reboot again to attempt to fix..."
+logmsg=$(date +"%D %T")" || Detected memory $mem is less than desired 15000. The script has rebooted the system $counter times to resolve this >
 echo $logmsg
 echo $logmsg>> $logpath
 reboot
 fi
 fi
+
